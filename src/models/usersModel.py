e5dfb7c5432357009596dbc1982b3a327dea7598
mlevitorresv <levitorres66@gmail.com>
2024-01-26 09:51:54 +0100

    ADD: parsing all numbers

--- src/models/usersModel.py
+++ src/models/usersModel.py
@@ -14,12 +14,22 @@ class UsersModel(Model):
     }
 
     def create(self):
-        self.data['photo'] = input('Enter photo: ')
-        self.data['id'] = int(input('Enter id: '))
+        self.data['photo'] = input('Enter photo: ')   
+        while True:
+            try:
+                self.data['id'] = int(input('Enter id: '))
+                break
+            except ValueError:
+                print('Error: Enter a valid id')
         self.data['name'] = input('Enter name: ')
         self.data['date'] = input('Enter date: ')
         self.data['email'] = input('Enter email: ')
-        self.data['phone'] = input('Enter phone: ')
+        while True:
+            try:
+                self.data['phone'] = input('Enter phone: ')
+                break
+            except ValueError:
+                print('Error: Enter a valid phone')
         self.data['description'] = input('Enter description: ')
         self.data['status'] = input('Enter status: ')
         print(f'the data was collected correctly \n {self.data}')
