625e4b1f9272c039ccb33f23aa17e808ff841f5b
mlevitorresv <levitorres66@gmail.com>
2024-01-26 10:14:10 +0100

    ADD: parsing all numbers on contacts

--- src/models/contactsModel.py
+++ src/models/contactsModel.py
@@ -15,11 +15,23 @@ class ContactsModel(Model):
 
     def create(self):
         self.data['photo'] = input('Enter photo: ')
-        self.data['id'] = int(input('Enter id: '))
+        
+        while True:
+            try:
+                self.data['id'] = int(input('Enter id: '))
+                break
+            except ValueError:
+                print('Error: Enter a valid id')
         self.data['name'] = input('Enter name: ')
         self.data['date'] = input('Enter date: ')
         self.data['email'] = input('Enter email: ')
-        self.data['phone'] = input('Enter phone: ')
+        
+        while True:
+            try:
+                self.data['phone'] = int(input('Enter phone: '))
+                break
+            except ValueError:
+                print('Error: Enter a valid phone')        
         self.data['description'] = input('Enter description: ')
         self.data['status'] = input('Enter status: ')
         print(f'the data was collected correctly \n {self.data}')
