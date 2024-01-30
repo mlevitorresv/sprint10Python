7bd75eaf309ad41f407223130c175024dfdafe6b
mlevitorresv <levitorres66@gmail.com>
2024-01-30 15:37:51 +0100

    ADD: .ENV

--- src/config/sql.py
+++ src/config/sql.py
@@ -1,10 +1,19 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_database = os.getenv("DB_DATABASE")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
 
 mydb = mysql.connector.connect(
-    host = "localhost",
-    database='dashboardhotelmiranda',
-    user = "root",
-    password = "12345678"
+    host = db_host,
+    database = db_database,
+    user = db_user,
+    password = db_password
 )
 
 print(mydb)
